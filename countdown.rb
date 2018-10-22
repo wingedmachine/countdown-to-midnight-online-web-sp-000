b5c56b7659afe128dd2543a3d87f0ebaddb7c61d
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 20:53:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(seconds)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
+
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1
+      sleep(1)
+    end
+    "HAPPY NEW YEAR!"
+end
